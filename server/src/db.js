532c7d70f7296a26a6584d36aa6a123c910842a4
unknown <sammitra47@gmail.com>
2018-10-10 12:45:15 -0700

    remove db.js

--- server/src/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-mongoose.Promise = global.Promise
-
-export const connect = () => {
-  return mongoose.connect('mongodb://samo:admin12@ds159782.mlab.com:59782/dbforme', {
-    useMongoClient: true
-  })
-}
