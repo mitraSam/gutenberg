8d7975106bce7531cf65f139f170db5890a61e3d
unknown <sammitra47@gmail.com>
2018-10-15 08:25:40 -0700

    delete seed & books

--- seed.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	"undefined": "mongodb://samo:admin12@ds159782.mlab.com:59782/dbforme",
-	"dev": "mongodb://samo:admin12@ds159782.mlab.com:59782/dbforme",
-	"prod": "mongodb://samo:admin12@ds159782.mlab.com:59782/dbforme"
-}
