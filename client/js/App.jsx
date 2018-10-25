9db9c8b997ad12bc3f7b98df4cd82724f4b77884
unknown <sammitra47@gmail.com>
2018-10-26 02:50:23 +0300

    refactor App component -> from class to component

--- client/js/App.jsx
+++ client/js/App.jsx
@@ -1,111 +1,101 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from "react";
+import React from "react";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../store";
 import ForFor from "./ForFor";
 import AsyncRoute from "./AsyncRoute";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Provider store={store}>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              component={props => (
-                <AsyncRoute
-                  props={props}
-                  path="landing"
-                  loadingComponent={import("../containers/LandingContainer")}
-                />
-              )}
+const App = () => (
+  <BrowserRouter>
+    <Provider store={store}>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={props => (
+            <AsyncRoute
+              props={props}
+              path="landing"
+              loadingComponent={import("../containers/LandingContainer")}
             />
-            <Route
-              exact
-              path="/about"
-              component={props => (
-                <AsyncRoute
-                  path="about"
-                  props={props}
-                  loadingComponent={import("./About")}
-                />
-              )}
+          )}
+        />
+        <Route
+          exact
+          path="/about"
+          component={props => (
+            <AsyncRoute
+              path="about"
+              props={props}
+              loadingComponent={import("./About")}
             />
-            <Route
-              exact
-              path="/book/:title"
-              component={props => (
-                <AsyncRoute
-                  props={props}
-                  path="details"
-                  loadingComponent={import("./BookDetails")}
-                />
-              )}
+          )}
+        />
+        <Route
+          exact
+          path="/book/:title"
+          component={props => (
+            <AsyncRoute
+              props={props}
+              path="details"
+              loadingComponent={import("./BookDetails")}
             />
-            <Route
-              path="/book/:title/read/:chapter"
-              component={props => (
-                <AsyncRoute
-                  props={props}
-                  loadingComponent={import("./BookContent")}
-                />
-              )}
+          )}
+        />
+        <Route
+          path="/book/:title/read/:chapter"
+          component={props => (
+            <AsyncRoute
+              props={props}
+              loadingComponent={import("./BookContent")}
             />
-            <Route
-              path="/book/:title/read"
-              component={props => (
-                <AsyncRoute
-                  props={props}
-                  loadingComponent={import("./BookContent")}
-                />
-              )}
+          )}
+        />
+        <Route
+          path="/book/:title/read"
+          component={props => (
+            <AsyncRoute
+              props={props}
+              loadingComponent={import("./BookContent")}
             />
-            <Route
-              path="/search/:searchTerm"
-              component={props => (
-                <AsyncRoute
-                  props={props}
-                  loadingComponent={import("../containers/SearchContainer")}
-                />
-              )}
+          )}
+        />
+        <Route
+          path="/search/:searchTerm"
+          component={props => (
+            <AsyncRoute
+              props={props}
+              loadingComponent={import("../containers/SearchContainer")}
             />
-            <Route
-              exact
-              path="/signin"
-              component={props => (
-                <AsyncRoute
-                  props={props}
-                  loadingComponent={import("./SignIn")}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/signup"
-              component={props => (
-                <AsyncRoute
-                  props={props}
-                  loadingComponent={import("./SignUp")}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/user"
-              component={props => (
-                <AsyncRoute props={props} loadingComponent={import("./User")} />
-              )}
-            />
+          )}
+        />
+        <Route
+          exact
+          path="/signin"
+          component={props => (
+            <AsyncRoute props={props} loadingComponent={import("./SignIn")} />
+          )}
+        />
+        <Route
+          exact
+          path="/signup"
+          component={props => (
+            <AsyncRoute props={props} loadingComponent={import("./SignUp")} />
+          )}
+        />
+        <Route
+          exact
+          path="/user"
+          component={props => (
+            <AsyncRoute props={props} loadingComponent={import("./User")} />
+          )}
+        />
 
-            <Route component={ForFor} />
-          </Switch>
-        </Provider>
-      </BrowserRouter>
-    );
-  }
-}
+        <Route component={ForFor} />
+      </Switch>
+    </Provider>
+  </BrowserRouter>
+);
 
 export default App;
